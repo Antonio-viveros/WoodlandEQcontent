9f812634543d1db0f52351413010ed93bc014ae3
Antonio-viveros <antonioviveros03@icloud.com>
2026-04-24 15:23:39 -0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 40 }}>
       <h1>WoodlandEQcontent</h1>
-      <p>Build test running successfully</p>
+      <p>Build running correctly</p>
     </main>
   );
 }
